fa718ab0ace68148db6324d502c322818e353463
KarenG2907 <karengaleano1138170@correo.itm.edu.co>
2026-03-15 19:10:03 -0500

    Comentarios

--- TheMind.cpp
+++ TheMind.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
+#include <vector>
+
+
+
 
 class Partida{
     
     //Atributos
 
+
     private:
         int nivel;
-        int cartasJugando; //cartas en juego en la partida
+        vector<int> cartasJugando; //cartas en juego en la partida
         int jugadores;
         int vidas;
         int shuriken;
@@ -16,16 +21,35 @@ class Partida{
 
     public:
         Partida();
-        int repartirCartas();
-        bool verificacionPartida();
-        int modificacionNivel(int nivel_);
-        int modificacionVidas(int vidas_);
-        int modificacionShuriken(int shuriken_);
+        /*define los atributos de la clase partida, define el vector de jugadores 
+        (crea la cantidad de jugadores que va a tener)*/
+        int repartirCartas(int nivel,vector<Jugador> jugadores); 
+        // Asigna los números aleatorios sin repetir a cada jugador, recibe el numero de nivel
+        bool cartaLanzada(int cartaLanzada);
+        /*verifica que la carta lanzada si sea la menor entre todas las 
+        cartas de los jugadores. Si no es menor, descarta las cartas menores, 
+        las elimina del array de cartas del 1 al 100, 
+        resta una vida, y agrega la carta lanzada al array de cartas jugando*/
+        bool verificacionPartida(const vector<int>& cartasJugando); 
+        /*Se recibe el vector de las cartas lanzadas por los jugadores 
+        y se verifica que esté en orden ascendente y retorna true o false 
+        para ganada o perdida la partida*/
+        bool usarShuriken(int usoShuriken);
+        /*Se descartan la carta de menor valor del mazo de cada jugador, modificando 
+        el vector de las cartas de los jugadores*/
+        int modificacionNivel(bool validador);
+        /*suma nivel set&get*/
+        int modificacionVidas(bool validador);
+        /*suma o resta de vidas set&get*/
+        int modificacionShuriken(bool validador);
+        /*suma o resta de shuriken set&get*/
 
 
 };
 
+Partida::Partida(){
 
+};
 
 
 class Jugador{
@@ -33,14 +57,36 @@ class Jugador{
     //Atributos
     private:
         string nombre;
-        int cantCartas;
-        int lasCartas; //Exactamente las cartas que tiene el jugador (qué numeros tiene en las cartas de su mano)
+        int numJugador;
+        vector<int> lasCartas; 
+        /*Exactamente las cartas que tiene el jugador (qué numeros tiene en las cartas de su mano)*/
     
     //Métodos
     public:
         Jugador();
+        /*defino el nombre del jugador y el número del jugador*/
         int lanzarCarta();
+        /*Retorna la menor carta del mazo del jugador*/
         int recibirCarta();
+        /*Recibe el vector con los números repartidos y se asigna al vector lasCartas*/
 
 
 };
+
+int main(){
+
+    /*
+    - cout Hola ¿cuántos jugadores van a participar?
+        Este valor se va a guardar en la variable jugadores
+    - con el número de jugadores creo el vector jugadores que se va a usar en la clase Jugador
+    - Se asignan las cartas y se muestra para cada jugador sus cartas
+    - Lanzar carta (1) o usar sguriken(0)
+    - Preguntar qué jugador va a lanzar
+    - Mostrar carta lanzada (verificando la menor del mazo)
+    
+    
+    */
+
+
+
+}
