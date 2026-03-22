d34da8b7497a959b78bd1efbc153191cf30f0bec
KarenG2907 <karengaleano1138170@correo.itm.edu.co>
2026-03-21 19:24:29 -0500

    Actualización

--- Jugador.cpp
+++ Jugador.cpp
@@ -1,8 +1,5 @@
 #include "Jugador.h"
-<<<<<<< HEAD
 #include <vector>
-=======
->>>>>>> 80778cf019d24a813c7e51d7cefea29f5801a704
 using namespace std;
 
 //nombre del jugador
@@ -10,11 +7,14 @@ Jugador::Jugador(string i){
 	nombre=i;//ingreso de nombre del jugador
 }
 
-//recibir cartas
-void Jugador::recibirCarta(int carta){
-		lasCartas.push_back(carta);
+/*Se reciben las cartas que proviene del método repartirCartas*/
+void Jugador::recibirCarta(vector<int> mano){
+	
+	lasCartas = mano;
 	
 }
+
+
 int Jugador::lanzarCarta(){
 	int carta = lasCartas.back();//guardamos carta
 	lasCartas.pop_back();//quitamos la carta que lanzamos del vector
